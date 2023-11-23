d05a9ee58d33f8769620da1b517545acac72f2c9
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2023-11-24 01:17:50 +0800

    init

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,18 +6,14 @@ pluginManagement {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
-//plugins {
-//    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
-//}
-
-//dependencyResolutionManagement {
-//    repositories {
-//        mavenCentral()
-//        google()
-//        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-//    }
-//}
-
+dependencyResolutionManagement {
+    // https://youtrack.jetbrains.com/issue/KT-51379
+    // repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
 rootProject.name = "kown"
 
 include("library")
